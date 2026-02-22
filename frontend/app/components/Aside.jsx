ba59074055077ef74115d16bc94606b0c1fd7160
MW-Limah <cdrmw16@gmail.com>
2026-02-22 19:14:01 +0000

    Framer motion na aside bar

--- frontend/app/components/Aside.jsx
+++ frontend/app/components/Aside.jsx
@@ -1,39 +1,48 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaHome, FaTruckLoading, FaBoxes } from "react-icons/fa";
 import { FaPeopleCarryBox } from "react-icons/fa6";
+import { motion } from "framer-motion";
+
+const pages = [
+  { page: "/", icon: FaHome, label: "Visão geral" },
+  { page: "/products", icon: FaBoxes, label: "Produtos" },
+  { page: "/suppliers", icon: FaTruckLoading, label: "Fornecedores" },
+  { page: "/associations", icon: FaPeopleCarryBox, label: "Associações" },
+];
 
 export default function Aside() {
+  const pathname = usePathname();
+
   return (
-    <div className="flex px-4 py-2 flex-col h-screen w-[20rem] bg-white text-[#2d2d2d] border-r-1 border-[#999]">
-      <h1 className="mt-4 pb-2 text-2xl border-b-1 border-[#999] ">
-        Invsys Controller
-      </h1>
-      <ul className="flex flex-col mt-4 text-xl gap-2">
-        <Link href={"/"}>
-          <li className="flex gap-4 items-center">
-            <FaHome />
-            Visão geral
-          </li>
-        </Link>
-        <Link href={"/suppliers"}>
-          <li className="flex gap-4 items-center">
-            <FaTruckLoading />
-            Fornecedores
-          </li>
-        </Link>
-        <Link href={"/products"}>
-          <li className="flex gap-4 items-center">
-            <FaBoxes />
-            Produtos
-          </li>
-        </Link>
-        <Link href={"/associations"}>
-          <li className="flex gap-4 items-center">
-            <FaPeopleCarryBox />
-            Associações
-          </li>
-        </Link>
+    <aside className="flex px-4 py-2 flex-col h-screen w-[20rem] bg-white text-[#2d2d2d] border-r border-[#999]">
+      <h1 className="mt-4 pb-2 text-2xl border-b border-[#999]">Invsys Controller</h1>
+
+      <ul className="flex flex-col gap-[10px] py-4 px-2 text-xl">
+        {pages.map((page) => {
+          const isActive = pathname === page.page;
+
+          return (
+            <Link key={page.page} href={page.page} className="relative group">
+              <li
+                className={`
+                relative z-10 flex items-center gap-3 py-2 px-4 rounded-md transition-colors duration-500
+                ${isActive ? "text-emerald-600" : "text-[#2d2d2d] hover:text-gray-500"}
+              `}
+              >
+                <page.icon size={22} />
+
+                {isActive && (
+                  <motion.div layoutId="activeTab" className="absolute inset-0 bg-emerald-500 w-[5px] -z-10" transition={{ type: "spring", bounce: 0.5, duration: 0.3 }} />
+                )}
+                <span className="font-medium">{page.label}</span>
+              </li>
+            </Link>
+          );
+        })}
       </ul>
-    </div>
+    </aside>
   );
 }
